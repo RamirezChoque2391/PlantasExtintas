613bdf9b97d2e5721be22cfe3a7f5a878cedc4ac
RamirezChoque2391 <hector.ramirez.choque@gmail.com>
2022-01-25 18:29:53 -0500

    Eliminamos esta carpeta

--- Ejemplotidy.R
+++
@@ -1,8 +0,0 @@
-#Activamos el paquete tidyverse
-library(tidyverse)
-
-#Cargamos unabase de datos
-data("mtcars")
-
-#Seleccionar solo los autos que tienen 8 cilindros
-mt<-mtcars %>% filter(cyl==8)
